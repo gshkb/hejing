00dc5d2e5acb40687fc32edeb2dc6a14e9b72eb2
何凯博 <w99699w@126.com>
2019-05-28 17:11:04 +0800

    MQ代码提交

---
+++ spring-elasticsearch/src/main/java/cn/gshkb/springelasticsearch/SpringElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package cn.gshkb.springelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticsearchApplication.class, args);
+	}
+
+}

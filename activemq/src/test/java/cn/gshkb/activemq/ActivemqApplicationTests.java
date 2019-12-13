1da4c0d43577f8a5bb7fb80e5763e132b70f6e59
hekb <w99699w@126.com>
2019-12-13 17:32:22 +0800

    final关键字的测试

--- activemq/src/test/java/cn/gshkb/activemq/ActivemqApplicationTests.java
+++ activemq/src/test/java/cn/gshkb/activemq/ActivemqApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.jms.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,5 +28,25 @@ public class ActivemqApplicationTests {
 		}
 
 	}
+
+    /**
+     *<P>
+     *     final 关键字的测试
+     *</P>
+     */
+    @Test
+    public void testFinal() {
+        final List<String> list = new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+
+        for (Object s : list) {
+            System.out.println(s);
+        }
+
+        final User user = new User("李四",11);
+        System.out.println(user);
+    }
+
 
 }
